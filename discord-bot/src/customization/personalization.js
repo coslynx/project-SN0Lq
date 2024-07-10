a387a52573d5a3df12e93f30ede84a124c12c529
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-10 11:05:41 +0530

    Generated: `discord-bot/src/customization/personalization.js`

---
+++ discord-bot/src/customization/personalization.js
@@ -0,0 +1,9 @@
+const personalizeSettings = (userId, avatar, theme) => {
+    // Logic to personalize user settings
+};
+
+const customizeGameSettings = (serverId, settings) => {
+    // Logic to customize game settings for a server
+};
+
+export { personalizeSettings, customizeGameSettings };
